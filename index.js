c799f29153ddd7d4ac3b3583a3337f597e7a121a
Moyboy <Vincent.moy94@gmail.com>
2024-06-28 14:10:59 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(recordArray){
+    const result = recordArray.find(function(record){
+        return record.result === "W"
+    })
+    if(result !== undefined){
+        return result.year
+    }
+    return undefined
+}
